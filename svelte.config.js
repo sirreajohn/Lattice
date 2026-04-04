8eda678013ee8dc73bf67fde442ca03cfc83d8a9
sirreajohn <mahesh.patapali@gmail.com>
2026-04-05 03:35:29 +0530

    vercel deployment options

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { relative, sep } from 'node:path';
 
 /** @type {import('@sveltejs/kit').Config} */
